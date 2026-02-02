2629e3eac8c871df25dcb51dd6cc121f991af57f
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:19 +0100

    fix offsets, add TODO

--- src/river2Dsoftware_main.c
+++ src/river2Dsoftware_main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 
 // TODO: (river2D #7) allow for other font colours
+// TODO: (river2D #17) allow for linebreaks and other escape codes
 void river2D_loadText
 (
     EngineData    *engine,
@@ -17,8 +18,8 @@ void river2D_loadText
     uint8_t       font,
     uint16_t      charsize,
     uint32_t      spacing,
-    uint32_t      offsetY,
-    uint32_t      offsetX
+    uint32_t      offsetX,
+    uint32_t      offsetY
 ){
     if(!engine->planes[font].data)
     {
