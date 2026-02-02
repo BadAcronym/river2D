893c9401b41dd69c6cd64d3ebb2cbc4d60b24e64
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:17 +0100

    unnecessary stuff in win32 header

--- include/win32_river2Dsoftware_platform.h
+++ include/win32_river2Dsoftware_platform.h
@@ -4,30 +4,4 @@
 #include <Xinput.h>
 
 #include <stdint.h>
-
-#define openWindow         win32openWindow
-#define drawFrame          win32drawFrame
-#define allocateBackbuffer win32allocateBackbuffer
-#define updateBackbuffer   win32updateBackbuffer
-#define BltBuffer          win32bltBuffer
-#define queryTime          win32queryTime
-
-typedef struct Win32WindowDimensions
-{
-    uint32_t width;
-    uint32_t height;
-}
-Win32WindowDimensions;
-
-typedef struct Win32Backbuffer
-{
-    BITMAPINFO info;
-    void       *memory;
-    uint32_t   width;
-    uint32_t   height;
-}
-Win32Backbuffer;
-
-extern void win32LoadXInput(void);
-
-extern Time win32QueryTime(void)
+#include <wdm.h>
