ecb2125cc0d4dae8e86c491f40da9647307c3644
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:19 +0100

    add warning for future time

--- src/river2Dcommon_main.c
+++ src/river2Dcommon_main.c
@@ -12,6 +12,11 @@ River2D_Time river2D_deltaTime
     River2D_Time current = river2D_queryTime();
     River2D_Time delta   = {0};
 
+    if(time->s > current.s || (time->ns > current.ns && time->s == current.s))
+    {
+        fprintf(stderr, "\033[33mWARNING: delta time lies in the future.\033[0m\n");
+    }
+
     delta.s = current.s - time->ns;
 
     if(current.ns >= time->ns)
