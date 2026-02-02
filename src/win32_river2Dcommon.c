d4ce5a5b59ba86ff23c83d7049a39aab2f837594
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:19 +0100

    handle windows keyboard

--- src/win32_river2Dcommon.c
+++ src/win32_river2Dcommon.c
@@ -43,23 +43,16 @@ uint8_t river2D_verifyPath
     return RIVER2D_TYPE_OTHER;
 }
 
-// TODO: fixup windows control interpreting
 uint8_t river2D_interpretCharAsKey
 (
     char inp
 ){
-    const uint8_t alphabetic_table[26] =
+    if(inp > 0x60 && inp < 0x7A)
     {
-        38, 56, 54, 40, 26, 41, 42, 43, 31, 44,
-        45, 46, 58, 57, 32, 33, 24, 27, 39, 28,
-        30, 55, 25, 53, 29, 52,
-    };
-
-    if(inp < 123 && inp > 96)
-    {
-        return alphabetic_table[inp - 97];
+        return inp - 32;
     }
-    return 0;
+
+    return inp;
 }
 
 Dimensions river2D_getWindowSize
