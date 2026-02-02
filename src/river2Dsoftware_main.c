95ebd5cc3e5ed1f20ccf3543fa58cc1ef1d094d4
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:18 +0100

    no need for stencil, diminishing returns

--- src/river2Dsoftware_main.c
+++ src/river2Dsoftware_main.c
@@ -9,9 +9,6 @@
 #include <string.h>
 
 //TODO: allow for other font colours?
-//maybe load image as some stencil boolean, then operate on the pixels (with desired colour)
-//based on that stencil
-//this could also be its own river2D_compositeText function
 void river2D_loadText
 (
     EngineData    *engine,
