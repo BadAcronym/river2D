ed080ee89b72ab078af76d9a9dc99b35382fb59b
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:18 +0100

    remove non-export func

--- include/linux_river2Dsoftware_platform.h
+++ include/linux_river2Dsoftware_platform.h
@@ -1,15 +1,3 @@
 #pragma once
 
 #include "river2D_main.h"
-
-extern Window river2D_openWindow
-(
-    EngineData *engine
-);
-
-extern void river2D_resizeBackbuffer
-(
-    EngineData *engine,
-    uint32_t   width,
-    uint32_t   height
-);
