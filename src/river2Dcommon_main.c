cfc01e7f9061b4252aeff1a80d34dfac37e61709
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:19 +0100

    bugfix deltaTime calculation

--- src/river2Dcommon_main.c
+++ src/river2Dcommon_main.c
@@ -10,27 +10,23 @@ River2D_Time river2D_deltaTime
     }
 
     River2D_Time current = river2D_queryTime();
-    River2D_Time delta   = {0};
+    int64_t      deltaS  = 0;
+    int64_t      deltaNS = 0;
 
     if(time->s > current.s || (time->ns > current.ns && time->s == current.s))
     {
         fprintf(stderr, "\033[33mWARNING: delta time lies in the future.\033[0m\n");
     }
 
-    delta.s = current.s - time->ns;
+    deltaS  = current.s  - time->s;
+    deltaNS = current.ns - time->ns;
 
-    if(current.ns >= time->ns)
+    if(deltaNS < 0)
     {
-        delta.ns = current.ns - time->ns;
-    }
-    else
-    {
-        delta.s -= 1;
-        delta.ns = (current.ns + 1e9) - time->ns;
+        deltaS  -= 1;
+        deltaNS += 1e9L;
     }
 
-    delta.s  = current.s  - time->s;
-    delta.ns = current.ns - time->ns;
-
+    River2D_Time delta = {deltaS, deltaNS};
     return delta;
 }
