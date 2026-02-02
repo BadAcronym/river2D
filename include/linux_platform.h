f18f6bb2c02da3f78b598916f550c6f4ebc33737
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:17 +0100

    window that opens & closes!

--- include/linux_platform.h
+++ include/linux_platform.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include "river2D_main.h"
+#include "X11/Xlib.h"
+
+#define openWindow         X11openWindow
+#define allocateBackbuffer X11allocateBackbuffer
+#define updateBackbuffer   X11updateBackbuffer
+#define bltBuffer          X11bltBuffer
+#define queryTime          X11queryTime
+
+#define Backbuffer         X11Backbuffer
+
+typedef struct X11Backbuffer
+{
+    void       *address;
+    Dimensions dimensions;
+}
+X11Backbuffer;
+
+extern Window X11openWindow
+(
+    Display     *display,
+    Dimensions  dimensions,
+    const char* windowName
+);
+
+//TODO: do we need this?
+extern Backbuffer* X11allocateBackbuffer
+(
+    Dimensions dimensions
+);
+
+extern void X11updateBackbuffer
+(
+    Backbuffer *buf
+);
+
+extern void X11bltBuffer
+(
+    Backbuffer *buf,
+    void       *window
+);
+
+extern uint64_t X11queryTime
+(
+);
--- src/linux_entry.c
+++ src/linux_entry.c
@@ -1,10 +1,48 @@
 #include "river2D_main.h"
+#include "linux_platform.h"
 
 #include <stdio.h>
 
 int main()
 {
-    printf("%s", "TODO");
+    Dimensions dimensions = {0};
+    dimensions.width = 640;
+    dimensions.height = 360;
 
+    Display *display = XOpenDisplay(NULL);
+
+    Window window = X11openWindow(display, dimensions, "river2D");
+
+    X11allocateBackbuffer(dimensions);
+
+    bool running = true;
+    while(running)
+    {
+        while(XPending(display) > 0)
+        {
+            XEvent event = {0};
+            XNextEvent(display, &event);
+            if(event.type == KeyPress)
+            {
+                //TODO: process keymap
+                printf("%s", "KeyPress\n");
+            }
+            else if(event.type == KeyRelease)
+            {
+                //TODO: process keymap
+                printf("%s", "KeyRelease\n");
+            }
+            else if(event.type == ClientMessage)
+            {
+                running = false;
+                break;
+            }
+        }
+
+        //TODO: work!
+    }
+
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
     return 0;
 }
