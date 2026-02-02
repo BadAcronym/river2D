f68ec23749208384f72428c5c0e50086e1b2dcfd
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:18 +0100

    move to top

--- src/win32_river2D_common.c
+++ src/win32_river2D_common.c
@@ -2,6 +2,19 @@
 
 #include <sys/stat.h>
 
+//TEST: verify if time outputs correctly
+void river2D_queryTime
+(
+    River2D_Time *time
+){
+    LARGE_INTEGER t1;
+
+    KeQuerySystemTimePrecise(&time);
+
+    time->s = t1.QuadPart / 10000000;
+    time->ns = t1.QuadPart * 100;
+}
+
 uint8_t river2D_verifyPath
 (
     const char *path
@@ -24,17 +37,4 @@ uint8_t river2D_verifyPath
     }
 
     return RIVER2D_TYPE_OTHER;
-}
-
-//TEST: verify if time outputs correctly
-void river2D_queryTime
-(
-    River2D_Time *time
-){
-    LARGE_INTEGER t1;
-
-    KeQuerySystemTimePrecise(&time);
-
-    time->s = t1.QuadPart / 10000000;
-    time->ns = t1.QuadPart * 100;
 }
