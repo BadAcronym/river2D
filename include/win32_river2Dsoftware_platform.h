db52e7c9250c5083ee1f7597da340008193716d8
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:33:17 +0100

    include all 3 listed by MSDN

--- include/win32_river2Dsoftware_platform.h
+++ include/win32_river2Dsoftware_platform.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include <Windows.h>
-#include <Xinput.h>
-
 #include <stdint.h>
+
+#include "Windows.h"
+#include "Xinput.h"
+
+#include "wdm.h"
+#include "ntddk.h"
+#include "ntifs.h"
